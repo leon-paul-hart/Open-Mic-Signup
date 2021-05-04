bc61087967ae7bd086422f55050d147c4d82b903
Leon-Paul Hart <leon.paul.hart@protonmail.com>
2021-05-04 16:30:24 +0100

    Added libman javascript/css packages to project and removed unused nuget packages

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -41,6 +36,7 @@ namespace Open_Mic_Signup
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
